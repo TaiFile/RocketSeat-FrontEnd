23f80a8369e4fabb737b1c6be8844e8caf1ecb61
Vitor Taira <vitor.taira@catijr.com.br>
2024-09-26 17:50:11 -0300

    começo react

---
+++ 1/01-FundamentosReactJs/src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from './components/header.jsx'
+import './global.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Teste</h1>
+    </div>
+  )
+}
+
+export default App;
